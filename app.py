0138152e6ceab0c179804ef3a91dbf20b8896de1
Самвел Цаканян <Samvel@ugra.tvr>
2025-07-01 16:02:28 +0500

    Добавил PostgeSQL - наконец заработала

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "mysecretkey"
-app.config['SQLACHEMY_DATABASE_URL'] = 'postgresql://samlion:333693@localhost/flaskdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://samlion:333693@localhost/flaskdb'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
     
 
 @app.route('/')
@@ -29,11 +32,42 @@ def register():
         if not username or not email or not password:
             flash('Заполните все поля!')
             return render_template('register.html')
+        
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        
         flash('Регистрация прошла успешно!')
         return redirect('/')
     
     return render_template('register.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+        else:
+            return 'Неверный логин или пароль'
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    user_id = session.get('user_id')
+    if not user_id:
+        flash('Пожалуйста, войдите в систему')
+        return redirect(url_for('login'))
+    
+    user = User.query.get(user_id)
+    return render_template('profile.html', user=user)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
